88c15caa75d99a9e3ac76764a61f4c418d0502b3
Orwell <34101369+Orwell1894@users.noreply.github.com>
2018-01-05 07:31:46 -0800

    Revert "Revert "Rev 1.1""
    This reverts commit 617da673944bc3a03799a6a5a54394351a1f807f.

--- HSOX.dpr
+++ HSOX.dpr
@@ -3,14 +3,14 @@ program HSOX;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  QTorUnit in 'QTorUnit.pas' {QTorWindow},
-  QTorModule in 'QTorModule.pas';
+  HSOXUnit in 'HSOXUnit.pas' {WHSOX},
+  HSOXModule in 'HSOXModule.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.FormFactor.Orientations := [TFormOrientation.Portrait];
-  Application.CreateForm(TQTorWindow, QTorWindow);
+  Application.CreateForm(TWHSOX, WHSOX);
   Application.Run;
 end.
